7b6ca106247f64148391b11e82eb1aed9d817b42
charles.chang <charles.chang@royaltek.com>
2020-11-24 16:21:32 +0800

    just record

--- read.cpp
+++ read.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <sstream>
+#include <map>
 #include <string>
 #include <vector>
 #include <boost/filesystem/string_file.hpp>
 #include <boost/algorithm/string.hpp>
 
 using namespace std;
+
+unsigned int str2ui(string strin)
+{
+	unsigned int ul = stoul(strin,nullptr,16);
+
+	return ul;
+}
 
 int main()
 {
@@ -15,12 +24,21 @@ int main()
 	boost::algorithm::split(linev, result, boost::is_any_of("\n"));
 
 	vector<string>::iterator lit=linev.begin()+1;
+	map<int,int> regs;
 	while(lit!=linev.end()){
+		unsigned int regadd,regvalue;
+		unsigned int data;
 		if(lit->find("Address write: 1A")!=string::npos){
 			lit+=2;
 			cout << lit->substr(lit->length()-2) << endl;
+			data = str2ui(lit->substr(lit->length()-2));
+			
+
 			lit+=2;
 			cout << lit->substr(lit->length()-2) << endl;
+			regvalue = str2ui(lit->substr(lit->length()-2));
+			cout <<  hex << data << ":" << hex << regvalue << endl;
+
 		}
 		lit++;
 	}
