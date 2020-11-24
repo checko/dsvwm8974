3a5cebd9e374b5870fb01d5f33d357e10931e075
charles.chang <charles.chang@royaltek.com>
2020-11-24 13:17:16 +0800

    split line

--- read.cpp
+++ read.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <boost/filesystem/string_file.hpp>
+#include <boost/algorithm/string.hpp>
 
 int main()
 {
 	std::string result;
 	boost::filesystem::load_string_file("dsvlog",result);
 	std::cout << result.size() << std::endl;
+
+	std::vector<std::string> linev;
+	boost::algorithm::split(linev, result, boost::is_any_of("\n"));
+	std::cout << linev[1] << std::endl;
+
+	for(std::string n : linev) {
+		std::cout << n <<std:: endl;
+	}
 
 	return 0;
 }
